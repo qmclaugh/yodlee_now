f35ad5fcdc8cff8814d757c705f245a45d2c5062
Quinn McLaughlin <quinn@coincidence.net>
2014-01-26 23:40:10 -0800

    Added user registration, financial site search, login and addition

--- lib/yodlee_now.rb
+++ lib/yodlee_now.rb
@@ -9,14 +9,15 @@ module YodleeNow
   YODLEE_API_URLS = {
     "END_POINT"=>
       {
-        "serviceBaseUrl"              =>  "https://rest.developer.yodlee.com/services/srest/restserver/v1.0", 
+        "sandboxBaseUrl"              =>  "https://rest.developer.yodlee.com/services/srest/restserver/v1.0", 
+        "stagingBaseUrl"              =>  "https://64.14.28.203/yodsoap/srest/sdkmaster/v1.0/", 
         "URL_COBRAND_SESSION_TOKEN"   =>  "/authenticate/coblogin", 
         "URL_USER_SESSION_TOKEN"      =>  "/authenticate/login",
         "URL_SEARCH_SITES"            =>  "/jsonsdk/SiteTraversal/searchSite",
         "URL_GET_SITE_LOGIN_FORM"     =>  "/jsonsdk/SiteAccountManagement/getSiteLoginForm",
         "URL_GET_ITEM_SUMMARIES"      =>  "/jsonsdk/DataService/getItemSummaries", 
         "URL_ADD_SITE_ACCOUNT"        =>  "/jsonsdk/SiteAccountManagement/addSiteAccount",
-        "foo" => 'bar'
+        "URL_REGISTER_USER"           =>  "/jsonsdk/UserRegistration/register3" 
       }
     } 
 
@@ -24,7 +25,7 @@ module YodleeNow
     attr_reader :response, :sessionToken, :error
     def login(cobrandLogin,cobrandPassword)
     
-      uri = URI.parse(YODLEE_API_URLS["END_POINT"]["serviceBaseUrl"]+YODLEE_API_URLS["END_POINT"]["URL_COBRAND_SESSION_TOKEN"])
+      uri = URI.parse(YODLEE_API_URLS["END_POINT"]["sandboxBaseUrl"]+YODLEE_API_URLS["END_POINT"]["URL_COBRAND_SESSION_TOKEN"])
       http = Net::HTTP.new(uri.host, uri.port)
       
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -51,8 +52,7 @@ module YodleeNow
   class User
     attr_reader :response, :sessionToken, :error
     def login(login,password,cobSessionToken)
-    
-      uri = URI.parse(YODLEE_API_URLS["END_POINT"]["serviceBaseUrl"]+YODLEE_API_URLS["END_POINT"]["URL_USER_SESSION_TOKEN"])
+      uri = URI.parse(YODLEE_API_URLS["END_POINT"]["sandboxBaseUrl"]+YODLEE_API_URLS["END_POINT"]["URL_USER_SESSION_TOKEN"])
       http = Net::HTTP.new(uri.host, uri.port)
       
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -74,13 +74,114 @@ module YodleeNow
       @error = json['Error']
       @error.nil? ? true : false
     end
+
+    def register(email,login,password,cobSessionToken)
+    
+      uri = URI.parse(YODLEE_API_URLS["END_POINT"]["sandboxBaseUrl"]+YODLEE_API_URLS["END_POINT"]["URL_REGISTER_USER"])
+      http = Net::HTTP.new(uri.host, uri.port)
+      
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      
+      if uri.scheme =='https'
+        http.use_ssl = true
+      end
+      
+      #TODO: DRY up http requests
+      res = http.post(uri.request_uri,"userCredentials.objectInstanceType=com.yodlee.ext.login.PasswordCredentials&userCredentials.loginName=#{login}&userCredentials.password=#{password}&userProfile.emailAddress=#{email}&cobSessionToken=#{cobSessionToken}")
+      json = JSON.parse(res.body)
+      @response = json
+      
+      if json['userContext']  && json['userContext']['conversationCredentials']['sessionToken']
+        @sessionToken = json['userContext']['conversationCredentials']['sessionToken']
+      else
+        @sessionToken = nil
+      end
+      @error = json['errorOccured']
+      @error == 'true' ? false : true # respond with "true" if no error has occured in the call
+    end
   end
 
+
+  class Site
+    attr_reader :response, :error
+    def search(cobSessionToken,userSessionToken,siteSearchString)
+      uri = URI.parse(YODLEE_API_URLS["END_POINT"]["sandboxBaseUrl"]+YODLEE_API_URLS["END_POINT"]["URL_SEARCH_SITES"])
+      http = Net::HTTP.new(uri.host, uri.port)
+      
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      
+      if uri.scheme =='https'
+        http.use_ssl = true
+      end
+      
+      #TODO: DRY up http requests
+      res = http.post(uri.request_uri,"cobSessionToken=#{cobSessionToken}&userSessionToken=#{userSessionToken}&siteSearchString=#{siteSearchString}")
+      json = JSON.parse(res.body)
+      @response=json
+
+      #TODO: Error handling.  Many types of responses from API here, some are JSON some not. For now, return true if call successful.
+      true
+
+    end
+
+    def basics
+      sites=[]
+      self.response.each do |res|
+        sites << [res['defaultDisplayName'],res['defaultOrgDisplayName'],res['siteId']]
+      end
+      return sites
+    end
+
+    def details(siteId)
+      res = self.response.select{|r| r['siteId'] == siteId}
+      res.first
+    end
+
+    def login_form(siteId)
+      res = self.details(siteId)
+      res['loginForms'].first['componentList']
+    end
+
+    def form_to_params(cobSessionToken,userSessionToken,siteId,form)
+      res = []
+      res << "cobSessionToken=#{CGI::escape cobSessionToken}"
+      res << "userSessionToken=#{CGI::escape userSessionToken}"
+      res << "siteId=#{siteId}"
+      form.each_with_index do |field,index|
+        field.each do |k,v|
+          if k =='fieldType'
+            res << "credentialFields%5B#{index}%5D.fieldType.typeName=#{CGI::escape v['typeName'].to_s}"
+          else
+            res << "credentialFields%5B#{index}%5D.#{k}=#{CGI::escape v.to_s}"
+          end
+        end
+      end
+      res << "credentialFields.enclosedType=com.yodlee.common.FieldInfoSingle"
+      res.join("&")
+    end
+
+    def add_account(cobSessionToken,userSessionToken,siteId,form)
+      parameters = form_to_params(cobSessionToken,userSessionToken,siteId,form)
+
+      uri = URI.parse(YODLEE_API_URLS["END_POINT"]["sandboxBaseUrl"]+YODLEE_API_URLS["END_POINT"]["URL_ADD_SITE_ACCOUNT"])
+      http = Net::HTTP.new(uri.host, uri.port)
+      
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      
+      if uri.scheme =='https'
+        http.use_ssl = true
+      end
+      res = http.post(uri.request_uri, parameters)
+      json = JSON.parse(res.body)
+      @response=json
+    end
+  end
+    
   class ItemSummaries
     attr_reader :response, :error
     def load(cobSessionToken,userSessionToken)
     
-      uri = URI.parse(YODLEE_API_URLS["END_POINT"]["serviceBaseUrl"]+YODLEE_API_URLS["END_POINT"]["URL_GET_ITEM_SUMMARIES"])
+      uri = URI.parse(YODLEE_API_URLS["END_POINT"]["sandboxBaseUrl"]+YODLEE_API_URLS["END_POINT"]["URL_GET_ITEM_SUMMARIES"])
       http = Net::HTTP.new(uri.host, uri.port)
       
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -129,6 +230,12 @@ module YodleeNow
       return [] if adata.nil? || adata.empty?
       adata['cardTransactions']
     end
+
+    def banking_transactions(institution_id,account_id)
+      adata = account_data(institution_id,account_id)
+      return [] if adata.nil? || adata.empty?
+      adata
+    end
     
     def card_transaction_basics(institution_id,account_id)
       txns = card_transactions(institution_id,account_id)
@@ -156,7 +263,6 @@ module YodleeNow
       end
       return txns_out
     end
-
   end
 
 end
